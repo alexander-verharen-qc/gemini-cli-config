e9f9883d9b019e66dbb9aeb8aa36a396ee06e667
alexander-verharen-qc <104868193+alexander-verharen-qc@users.noreply.github.com>
2025-06-30 19:20:26 -0700

    Delete pattern_packs/gcp.tf

--- pattern_packs/gcp.tf
+++
@@ -1,4 +0,0 @@
-option,description,syntax
-gke-service,Finds the deployment and service YAML for a GKE service.,"pattern: gke-service {name}\n  file: {name}/deployment.yaml\n  file: {name}/service.yaml"
-gcloud-logs,Tails the logs for a specific GCP project and log name.,"command: gcloud-logs {log_name}\n  exec: gcloud logging tail projects/your-gcp-project/logs/{log_name}"
-gcloud-auth,Prints the current gcloud authenticated user.,"command: gcloud-auth\n  exec: gcloud auth list --filter=status:ACTIVE --format='value(account)'"
